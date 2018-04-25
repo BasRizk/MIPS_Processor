afa479c790195b2f136f08a05610011102c99689
mike13k <mikegeorge2012@gmail.com>
2018-04-25 23:58:33 +0200

    fix internal size error in IF

--- instruction_fetch.sv
+++ instruction_fetch.sv
@@ -4,14 +4,15 @@ output reg [31:0] next_instruction, next_address;
 input [31:0] current_address;
 input clk;
 
-reg [7:0] i_mem [268435455:0];  // The size of the memory is based on 2^28. 28 are the bits PC[27:0] used as address index
+reg [7:0] i_mem [255:0];  // The size of the memory is based on 2^28. 28 are the bits PC[27:0] used as address index
+                          // The 2^28 does not work, so 255 is used for now
 
 always @(clk)
 begin    
     next_instruction <= {{i_mem[current_address[27:0]]},
-                        {i_mem[current_address[27:0] + 8'b0000_0001]},
-                        {i_mem[current_address[27:0] + 8'b0000_0010]},
-                        {i_mem[current_address[27:0] + 8'b0000_0011]}};
+                        {i_mem[current_address[27:0] + 1]},
+                        {i_mem[current_address[27:0] + 1]},
+                        {i_mem[current_address[27:0] + 1]}};
     next_address <= current_address + 4;
 end
 
