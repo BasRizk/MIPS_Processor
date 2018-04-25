88b9f3d21735eedeb7b049306347af9298c5a639
mike13k <mikegeorge2012@gmail.com>
2018-04-25 15:46:40 +0200

    change instruction fetch from IE to IF

--- main.sv
+++ main.sv
@@ -8,7 +8,7 @@ wire [31:0] alu_result, address_mem, read_data, wb_data;
 assign write_data = read_data_2;
 assign address_mem = alu_result;
 
-instruction_fetch IE (next_instruction ,next_address, new_address);
+instruction_fetch IF (next_instruction ,next_address, new_address);
 instruction_decode ID (read_data_1, read_data_2, extended_address, next_instruction, write_data);
 execute EX (new_address, zero, alu_result, next_address, read_data_1, read_data_2, extended_address);
 memory_access MEM(read_data, new_address, zero, address_mem, write_data);
