d82ef84d331c2b7d75eb8cdaa155937f0a1d0372
mike13k <mikegeorge2012@gmail.com>
2018-04-25 17:56:42 +0200

    add basic IF implementation

--- instruction_fetch.sv
+++ instruction_fetch.sv
@@ -1,9 +1,12 @@
 module instruction_fetch (next_instruction ,next_address, new_address);
-//Starting
-
 
 output reg [31:0] next_instruction, next_address;
 input [31:0] new_address;
 
+always @(new_address)
+begin
+    next_instruction <= new_address;
+    next_address = new_address + 1;
+end
 
 endmodule
