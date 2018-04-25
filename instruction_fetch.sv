a4616e01ed01417a37b0d536413c728218233510
mike13k <mikegeorge2012@gmail.com>
2018-04-25 19:46:38 +0200

    add some fixes

--- instruction_fetch.sv
+++ instruction_fetch.sv
@@ -1,21 +1,24 @@
-module instruction_fetch (next_instruction ,next_address, new_basic_address, new_branch_address, jump, clk);
+module instruction_fetch (next_instruction ,next_address, basic_address, jump_address, jump, clk);
 
 output reg [31:0] next_instruction, next_address;
 input [31:0] new_basic_address, new_branch_address;
 input clk, jump;
 
 reg [7:0] mem [27:0];
+reg [31:0] selected_address;
 
 always @(clk)
 begin    
-    next_instruction <= {{mem[new_address]}},
-                        {{mem[new_address + 8'b0000_0001]}},
-                        {{mem[new_address + 8'b0000_0010]}},
-                        {{mem[new_address + 8'b0000_0011]}};
-    case(jump)
-        1'b0 : next_address <= new_address + 8'b0000_0100;
-        1'b1 : next_address <= new_branch_address;
-    endcase
+    next_instruction <= {{mem[selected_address]}},
+                        {{mem[selected_address + 8'b0000_0001]}},
+                        {{mem[selected_address + 8'b0000_0010]}},
+                        {{mem[selected_address + 8'b0000_0011]}};
+    next_address <= selected_address + 4;
+end
+
+always @(jump || basic_address || jump_address)
+begin
+    selected_address = jump? basic_address : jump_address;
 end
 
 endmodule
