0b9aeadfed8e1b90492c9639f54ad78043d2da74
mike13k <mikegeorge2012@gmail.com>
2018-04-25 18:27:29 +0200

    get updates

--- instruction_fetch.sv
+++ instruction_fetch.sv
@@ -3,11 +3,12 @@ module instruction_fetch (next_instruction ,next_address, new_address, clk);
 output reg [31:0] next_instruction, next_address;
 input [31:0] new_address;
 input clk;
+reg [31:0] mem [67108863:0];
 
-always @(new_address)
-begin
-    next_instruction <= new_address;
-    next_address <= new_address + 1;
+always @(clk)
+begin    
+    next_instruction <= mem[new_address];
+    next_address <= new_address + 4;
 end
 
 endmodule
