7bc7a69d1c10f48673b8143e64ad67dbabde8fcc
mike13k <mikegeorge2012@gmail.com>
2018-04-25 19:22:51 +0200

    add fixes and implementation based on jump and mux

--- instruction_fetch.sv
+++ instruction_fetch.sv
@@ -1,14 +1,20 @@
-module instruction_fetch (next_instruction ,next_address, new_address, clk);
+module instruction_fetch (next_instruction ,next_address, new_basic_address, new_branch_address, jump, clk);
 
 output reg [31:0] next_instruction, next_address;
-input [31:0] new_address;
-input clk;
-reg [31:0] mem [67108863:0];
+input [31:0] new_basic_address, new_branch_address;
+input clk, jump;
+reg [7:0] mem [268435455:0];
 
 always @(clk)
 begin    
-    next_instruction <= mem[new_address];
-    next_address <= new_address + 4;
+    next_instruction <= {{mem[new_address]}},
+                        {{mem[new_address + 8'b0000_0001]}},
+                        {{mem[new_address + 8'b0000_0010]}},
+                        {{mem[new_address + 8'b0000_0011]}};
+    case(jump)
+        1'b0 : next_address <= new_address + 8'b0000_0100;
+        1'b1 : next_address <= new_branch_address;
+    endcase
 end
 
 endmodule
