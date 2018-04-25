20c16ab0f49d7082f2ae062a204b5f7d2ca29178
mike13k <mikegeorge2012@gmail.com>
2018-04-25 20:53:57 +0200

    some fixes to IF

--- instruction_fetch.sv
+++ instruction_fetch.sv
@@ -4,14 +4,14 @@ output reg [31:0] next_instruction, next_address;
 input [31:0] current_address;
 input clk;
 
-reg [7:0] mem [27:0];
+reg [7:0] i_mem [268435455:0];  // The size of the memory is based on 2^28. 28 are the bits PC[27:0] used as address index
 
 always @(clk)
 begin    
-    next_instruction <= {{mem[current_address]},
-                        {mem[current_address + 8'b0000_0001]},
-                        {mem[current_address + 8'b0000_0010]},
-                        {mem[current_address + 8'b0000_0011]}};
+    next_instruction <= {{i_mem[current_address[27:0]]},
+                        {i_mem[current_address[27:0] + 8'b0000_0001]},
+                        {i_mem[current_address[27:0] + 8'b0000_0010]},
+                        {i_mem[current_address[27:0] + 8'b0000_0011]}};
     next_address <= current_address + 4;
 end
 
