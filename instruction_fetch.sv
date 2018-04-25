0ecfb363e889975e260fae4808c0c223510218c0
mike13k <mikegeorge2012@gmail.com>
2018-04-25 20:11:52 +0200

    finish IF

--- instruction_fetch.sv
+++ instruction_fetch.sv
@@ -1,18 +1,18 @@
 module instruction_fetch (next_instruction ,next_address, current_address, clk);
 
 output reg [31:0] next_instruction, next_address;
-input [31:0] new_basic_address, new_branch_address;
-input clk, jump;
+input [31:0] current_address;
+input clk;
 
 reg [7:0] mem [27:0];
 
 always @(clk)
 begin    
-    next_instruction <= {{mem[current_address]}},
-                        {{mem[current_address + 8'b0000_0001]}},
-                        {{mem[current_address + 8'b0000_0010]}},
-                        {{mem[current_address + 8'b0000_0011]}};
-    next_address <= selected_address + 4;
+    next_instruction <= {{mem[current_address]},
+                        {mem[current_address + 8'b0000_0001]},
+                        {mem[current_address + 8'b0000_0010]},
+                        {mem[current_address + 8'b0000_0011]}};
+    next_address <= current_address + 4;
 end
 
 endmodule
