83d5a10df2f0ad4960faa879fa444e1694e7843e
Basem Rizk <basem.rizk@outlook.com>
2018-04-25 17:52:59 +0200

    alu_result to be (reg), solving error

--- ALU_Unit.sv
+++ ALU_Unit.sv
@@ -1,7 +1,8 @@
 module ALU_Unit(ALU_result,input1,input2,selectionLines);
-    output [31:0] ALU_result;
+    output reg [31:0] ALU_result;
     input [31:0] input1,input2;
     input [3:0] selectionLines;
+
     always@(input1,input2,selectionLines) begin
         case(selectionLines)
             4'b0000 : ALU_result = input1 & input2;
