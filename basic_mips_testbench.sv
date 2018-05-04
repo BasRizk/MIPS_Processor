ae1f41999d4e7a72af701dde0e8844f5a7448d6a
mike13k <mikegeorge2012@gmail.com>
2018-05-04 18:10:27 +0200

    add in test bench instructions for testing lh and lhu

--- basic_mips_testbench.sv
+++ basic_mips_testbench.sv
@@ -67,6 +67,86 @@ begin
 	// lw $16, 0($10) expected $16 = memory[(0*4) + 10] = 21
 	$display("lw $16, 0($10) expected $16 = memory[(0*4) + 10]");
 	instruction_mem[55:52] = '{8'h8d, 8'h50, 8'h00, 8'h00};
+
+	// addi $19, $0, 32767 expected $19 = 0x0000_007f
+	$display("addi $19, $0, 32767 expected $19 = 0x0000 0000 7 f");
+	instruction_mem[59:56] = '{8'h20, 8'h13, 8'h7f, 8'hff};
+
+	$display("NOP");
+	instruction_mem[63:60] = '{8'h00, 8'h00, 8'h00, 8'h00};
+
+	$display("NOP");
+	instruction_mem[67:64] = '{8'h00, 8'h00, 8'h00, 8'h00};
+
+	$display("NOP");
+	instruction_mem[71:68] = '{8'h00, 8'h00, 8'h00, 8'h00};
+
+	// addi $19, $0, 24576 expected $19 = 0x0001 ffff
+	$display("addi $19, $0, 24576 expected $19 = 0x0001 ffff");
+	instruction_mem[75:72] = '{8'h22, 8'h73, 8'h60, 8'h00};
+
+	$display("NOP");
+	instruction_mem[79:76] = '{8'h00, 8'h00, 8'h00, 8'h00};
+
+	$display("NOP");
+	instruction_mem[83:80] = '{8'h00, 8'h00, 8'h00, 8'h00};
+
+	$display("NOP");
+	instruction_mem[87:84] = '{8'h00, 8'h00, 8'h00, 8'h00};
+
+	instruction_mem[91:88] = '{8'h22, 8'h73, 8'h60, 8'h00};
+
+	$display("NOP");
+	instruction_mem[95:92] = '{8'h00, 8'h00, 8'h00, 8'h00};
+
+	$display("NOP");
+	instruction_mem[99:96] = '{8'h00, 8'h00, 8'h00, 8'h00};
+
+	$display("NOP");
+	instruction_mem[103:100] = '{8'h00, 8'h00, 8'h00, 8'h00};
+
+	instruction_mem[107:104] = '{8'h22, 8'h73, 8'h60, 8'h00};
+
+	$display("NOP");
+	instruction_mem[111:108] = '{8'h00, 8'h00, 8'h00, 8'h00};
+
+	$display("NOP");
+	instruction_mem[115:112] = '{8'h00, 8'h00, 8'h00, 8'h00};
+
+	$display("NOP");
+	instruction_mem[119:116] = '{8'h00, 8'h00, 8'h00, 8'h00};
+
+	instruction_mem[123:120] = '{8'h22, 8'h73, 8'h60, 8'h00};
+
+	$display("NOP");
+	instruction_mem[127:124] = '{8'h00, 8'h00, 8'h00, 8'h00};
+
+	$display("NOP");
+	instruction_mem[131:128] = '{8'h00, 8'h00, 8'h00, 8'h00};
+
+	$display("NOP");
+	instruction_mem[135:132] = '{8'h00, 8'h00, 8'h00, 8'h00};
+
+	// sw $19, 0($10) expected memory[(0*4) + 10] = 0x0001ffff
+	$display("sw $19, 0($10) expected memory[10] = 0x0001ffff");
+	instruction_mem[139:136] = '{8'had, 8'h53, 8'h00, 8'h00};
+
+	$display("NOP");
+	instruction_mem[143:140] = '{8'h00, 8'h00, 8'h00, 8'h00};
+
+	$display("NOP");
+	instruction_mem[147:144] = '{8'h00, 8'h00, 8'h00, 8'h00};
+
+	$display("NOP");
+	instruction_mem[151:148] = '{8'h00, 8'h00, 8'h00, 8'h00};
+
+	// lh $17, 0($10) expected $17 = 0xffff_ffff
+	$display("lh $17, 0($10) expected $17 = 0xffffffff");
+	instruction_mem[155:152] = '{8'h85, 8'h51, 8'h00, 8'h00};
+
+	// lhu $18, 0($10) expected $18 = 0x0000_ffff
+	$display("lhu $18, 0($10) expected $18 = 0x0000ffff");
+	instruction_mem[159:156] = '{8'h95, 8'h52, 8'h00, 8'h00};
 	
 	forever #100 clk = ~clk;
 end
