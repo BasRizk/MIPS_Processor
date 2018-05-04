602e0686c0791a2d560a092833782fa1c56e87a0
Basem Rizk <basem.rizk@outlook.com>
2018-05-04 23:50:45 +0200

    shamt added

--- ALU_Unit.sv
+++ ALU_Unit.sv
@@ -1,8 +1,9 @@
-module ALU_Unit(ALU_result,input1,input2,selectionLines, clk);
+module ALU_Unit(ALU_result,input1,input2,selectionLines, shamt, clk);
     output reg [31:0] ALU_result;
 
     input [31:0] input1,input2;
     input [3:0] selectionLines;
+    input [4:0] shamt;
     input clk;
     
     always@(*) begin
@@ -13,8 +14,8 @@ module ALU_Unit(ALU_result,input1,input2,selectionLines, clk);
             4'b0110 : ALU_result = input1 - input2;
             4'b1011 : ALU_result = (input1 < input2)? 1 : 0;    //sltu
             4'b0111 : ALU_result = ($signed(input1) < $signed(input2))? 1 : 0;   //slt
-            4'b1000 : ALU_result = input1 << input2;
-            4'b1001 : ALU_result = input2 >> input2;
+            4'b1000 : ALU_result = input2 << shamt;
+            4'b1001 : ALU_result = input2 >> shamt;
         endcase
     end
     
--- execute.sv
+++ execute.sv
@@ -21,14 +21,17 @@ module execute (
 
     reg [3:0] ALU_control;
     reg [31:0] ALU_input2;
+    reg [4:0] shamt;
     
     reg [31:0] alu_mid_result;
+    
 
-
-    ALU_Unit alu(alu_mid_result,read_data_1_id_ex,ALU_input2,ALU_control,clk);
-
+    ALU_Unit alu(alu_mid_result,read_data_1_id_ex,ALU_input2,ALU_control, shamt,clk);
+    
     assign  ALU_input2 = (ctrl_aluSrc_id_ex == 0)?
             read_data_2_id_ex : {0,extended_branch_offset_id_ex[15:0]};
+
+    assign shamt = extended_branch_offset_id_ex[10:6];
 
     always@(*) begin
 
@@ -58,7 +61,7 @@ module execute (
         ALU_result = alu_mid_result;
 
         branch_or_not_address =
-            supposed_next_address_id_ex + extended_branch_offset_id_ex * 4; 
+            supposed_next_address_id_ex + (extended_branch_offset_id_ex * 4); 
 
         // ctrl_regDest MUX using ctrl_regDest coming from ID/EX Pipeline Register
         write_register = (ctrl_regDest_id_ex)?
